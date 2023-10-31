f960b6aa3ee97c5602f81e4ed5450c444fabdef8
louisbarthelemy <louis.barthelemy@etu.minesparis.psl.eu>
2023-10-31 16:25:20 +0100

    histogramme distribution des températures

--- exoplanete.py
+++ exoplanete.py
@@ -23,21 +23,28 @@ df.set_index('pl_name')
 
 df.columns
 
-# +
-#pd.cut(df['pl_eqt'], bins = 
-# -
+
 
 df3 = df.dropna(subset = ['pl_eqt'])
 df3
 
+df3_1 = df3.set_index('pl_name')
+df3_1
+
 df3['pl_eqt'].describe()
 
-df4 = pd.cut(df3['pl_eqt'], bins = [0,500,1000,1500,2000,2500,3000,3500,4000,4500], labels= ['0-500 K', '500-1000 K', '1000-1500 K', '1500-2000 K', '2000-2500 K', '2500-3000 K', '3000-3500 K', '3500-4000 K', '4000-4500 K'])
+df4 = pd.cut(df3_1['pl_eqt'], bins = [0,500,1000,1500,2000,2500,3000,3500,4000,4500], labels= ['0-500 K', '500-1000 K', '1000-1500 K', '1500-2000 K', '2000-2500 K', '2500-3000 K', '3000-3500 K', '3500-4000 K', '4000-4500 K'])
 
-df4
+df3.hist('pl_eqt', bins = 200, color = 'blue')
 
-df['pl_eqt']
+# +
+#df4.plot(kind='hist', bins=9, edgecolor='k')
+# -
 
-df
+df5 = df4.value_counts()
+df5
+
+df5.plot.hist()
 
 
+df3.columns
