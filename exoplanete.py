b048a521975c788ac7e292d1b2bc54ebfa2c3365
louisbarthelemy <louis.barthelemy@etu.minesparis.psl.eu>
2023-10-31 15:34:36 +0100

    bins avec température

--- exoplanete.py
+++ exoplanete.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("PS_2023.10.31_06.00.18.csv", skiprows=96)
 
@@ -20,20 +21,23 @@ df2
 
 df.set_index('pl_name')
 
+df.columns
 
+# +
+#pd.cut(df['pl_eqt'], bins = 
+# -
 
+df3 = df.dropna(subset = ['pl_eqt'])
+df3
 
+df3['pl_eqt'].describe()
 
+df4 = pd.cut(df3['pl_eqt'], bins = [0,500,1000,1500,2000,2500,3000,3500,4000,4500], labels= ['0-500 K', '500-1000 K', '1000-1500 K', '1500-2000 K', '2000-2500 K', '2500-3000 K', '3000-3500 K', '3500-4000 K', '4000-4500 K'])
 
+df4
 
+df['pl_eqt']
 
-
-
-
-
-
-
-
-
+df
 
 
