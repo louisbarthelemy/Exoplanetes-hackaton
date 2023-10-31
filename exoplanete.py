4fd2e825f07fa5d0c6bdbe6e1a292cbc7c4c6698
louisbarthelemy <louis.barthelemy@etu.minesparis.psl.eu>
2023-10-31 15:06:51 +0100

    dimensions + exoplanete avec toutes les infos

--- exoplanete.py
+++ exoplanete.py
@@ -4,6 +4,36 @@ df = pd.read_csv("PS_2023.10.31_06.00.18.csv", skiprows=96)
 
 df
 
+df.describe()
+
+df['disc_year']
+
+df.columns
+
+# +
+df2 = df.dropna()
+
+# on garde dans df2 seulement les exoplanètes pour lesquelles on a toutes les informati
+# -
+
+df2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
